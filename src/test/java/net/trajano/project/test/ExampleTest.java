59327474299d6c7a26f3acdd98d46edff49a7795
Archimedes Trajano <arch@trajano.net>
2016-11-08 23:33:43 -0500

    X

---
+++ src/test/java/net/trajano/project/test/ExampleTest.java
@@ -0,0 +1,16 @@
+package net.trajano.project.test;
+
+import org.junit.Test;
+import net.trajano.project.Example;
+
+/**
+ * Example.
+ *
+ * @author Archimedes Trajano
+ */
+public class ExampleTest {
+    @Test
+    public void testSomething() {
+        new Example();
+    }
+}
